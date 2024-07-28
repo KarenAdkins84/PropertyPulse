bf01ab86a096f603da39bf68b02de0c5e665f03a
Nerakma84 <fnk8476@gmail.com>
2024-07-28 00:29:59 -0500

    footer component complete

--- components/InfoBoxes.jsx
+++ components/InfoBoxes.jsx
@@ -16,7 +16,18 @@ const InfoBoxes = () => {
                 backgroundColor: 'bg-black'
 
             }}>
-                Find your dream property. Bookmark properties and contact owners.
+                Find your dream rental property. Bookmark properties and contact owners.
+            </InfoBox>
+            <InfoBox
+            heading='For Property Owners'
+            backgroundColor='bg-blue-100'
+            buttonInfo={{
+                text: 'Add Property',
+                link: '/properties/add',
+                backgroundColor: 'bg-blue-500'
+
+            }}>
+                List your properties and reach potential tenants. Rent as an Airbnb or long term.
             </InfoBox>
         </div>
       </div>
